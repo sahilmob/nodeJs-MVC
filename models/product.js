345072caa747d4940486d6e9e4371c3232b1d1c7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-04 16:20:51 +0200

    save products to a file

--- models/product.js
+++ models/product.js
@@ -1,14 +1,36 @@
-const products = []
+const fs = require('fs');
+const path = require('path');;
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    products.push(this)
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (err) {
+        //We use callback function since the return is inside the read file hence the fetchAll func. does not return anything.
+        // return [] 
+        cb([]);
+      }
+      //We use callback function since the return is inside the read file hence the fetchAll func. does not return anything.
+      // return JSON.parse(fileContent)
+      cb(JSON.parse(fileContent));
+    });
   }
 }
